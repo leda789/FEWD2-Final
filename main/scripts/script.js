9438fb033a409dd84ce467ed168c400dc8587a94
priyarakkan <priyadharsinikdp@gmail.com>
2023-06-04 20:27:04 -0700

    Update script.js

--- main/scripts/script.js
+++ main/scripts/script.js
@@ -2,63 +2,40 @@
 <<<<<<< HEAD
 =======
 >>>>>>> be7747364244869ff41b9c96b0f7fb7735be963b
-/* Nav */
 
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-          nav   = document.getElementById(navId)
-
-    toggle.addEventListener('click', () => {
-
-        nav.classList.toggle('showMenu')
-
-        toggle.classList.toggle('showIcon')
-    })
-}
-
-showMenu('navToggle','navMenu');
-
-const dropdownItem = document.querySelectorAll('.dropdownItem')
-
-dropdownItem.forEach((item) => {
-    const dropdownButton = item.querySelector('.dropdownIcon')
-
-    dropdownButton.addEventListener('click', () => {
-
-        const showDropdown = document.querySelector('.showDropdown')
-
-        toggleItem(item)
-
-        if(showDropdown && showDropdown!= item) {
-            toggleItem(showDropdown)
-        }  
-    })
-
-})
-
-const toggleItem = (item) => {
-    const dropdownContent = item.querySelector('.dropdownContent')
-
-    if(item.classList.contains('showDropdown')){
-        dropdownContent.removeAttribute('style')
-        item.classList.remove('showDropdown')
-    } else {
-        dropdownContent.style.height = dropdownContent.scrollHeight + 'px'
-        item.classList.add('showDropdown')
-    } 
-}
-
-
-/* order */
-const orderBtn = document.querySelector('.button');
-const returnMessage = document.querySelector('.return');
-
-returnMessage.style.display ='none';
 <<<<<<< HEAD
 =======
 =======
 
 >>>>>>> be7747364244869ff41b9c96b0f7fb7735be963b
+
+
+
+/************
+ * NAV *
+ ************/
+const menuBtn = document.querySelector('.navbar .menuIcon');
+const closeBtn = document.querySelector('.navLinks .closeIcon')
+const navLinks = document.querySelector('.navLinks')
+
+menuBtn.addEventListener('click',() =>{
+    navLinks.style.left = '0';
+
+});
+closeBtn.addEventListener('click',() => {
+    navLinks.style.left = '-100%';
+})
+
+const submenuIcon = document.querySelector('.arrowIcon');
+submenuIcon.addEventListener('click', () =>{
+    navLinks.classList.toggle('show1');
+})
+
+
+
+
+
+
 /************
  * CAROUSEL *
  ************/
